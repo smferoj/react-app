a6f0a46e5acf85f80f88848da39ef68974e70f9c
smferoj <sm.feroj@gmail.com>
2024-01-19 08:04:48 +0600

    layout_complete

--- src/components/partials/BreadCrumb.jsx
+++ src/components/partials/BreadCrumb.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const BreadCrumb = (props) => {
   return (
     <> 
-    <h1 class="mt-4"> my Dashboard </h1>
+
     <ol class="breadcrumb mb-4">
+      <li class="breadcrumb-item ">Dashboard</li>
       <li class="breadcrumb-item active">{props.title}</li>
+      
     </ol>
     </>
   )
